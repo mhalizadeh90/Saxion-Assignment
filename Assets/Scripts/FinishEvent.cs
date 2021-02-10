5870e9a1aba12d53a82b56b5a6e878c1cce7fe8b
Mohammad Alizadeh <mhalizadeh@hotmail.com>
2021-02-10 13:30:36 +0330

    Start To Work On Maze Game

---
+++ Assets/Scripts/FinishEvent.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FinishEvent : MonoBehaviour
+{
+
+    void OnTriggerEnter2D(Collider2D collision)
+    {
+        OnBallReachFinishPoint?.Invoke();
+        print("Player Reached Finished");
+    }
+
+    public static Action OnBallReachFinishPoint;
+}
---
+++ Assets/Scripts/InputManager.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class InputManager : MonoBehaviour
+{
+    Slider InputSlider;
+    [SerializeField] FloatVariable InputValue;
+    bool IsSliderHold;
+
+    void Awake()
+    {
+        InputSlider = GetComponent<Slider>();
+        ResetInputValue();
+    }
+
+
+
+    public void ResetInputValue(float defaultValue = 0)
+    {
+        InputValue.value = defaultValue;
+        InputSlider.value = defaultValue;
+        IsSliderHold = false;
+        OnInputUpdate?.Invoke(false);
+        //print("Reset");
+    }
+
+    public void UpdateSliderValue()
+    {
+        InputValue.value = InputSlider.value;
+        
+        if(!IsSliderHold)
+        {
+            OnInputUpdate?.Invoke(true);
+            IsSliderHold = true;
+            //print("Update");
+        }
+    }
+
+    public static Action<bool> OnInputUpdate;
+
+}
