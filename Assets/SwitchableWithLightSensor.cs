628a7d87817ba9b5ea3ded93d7dd186f00391c6e
Mohammad Alizadeh <mhalizadeh@hotmail.com>
2021-03-03 13:44:05 +0330

    Finish Level 3

---
+++ Assets/SwitchableWithLightSensor.cs
@@ -0,0 +1,70 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SwitchableWithLightSensor : MonoBehaviour
+{
+    public enum OpenState { Open,Close }
+
+    [SerializeField] Lights Light;
+    [SerializeField] OpenState DefaultState;
+    OpenState currentSwitchState;
+    Collider2D itemCollider;
+    DissolveController dissolveController;
+
+    void Awake()
+    {
+        currentSwitchState = DefaultState;
+        itemCollider = GetComponent<Collider2D>();
+        dissolveController = GetComponent<DissolveController>();
+        
+        if (DefaultState == OpenState.Open)
+            Open();
+        else if (DefaultState == OpenState.Close)
+            Close();
+    }
+
+    void OnEnable()
+    {
+        LightSensor.OnLightSensorTriggered += ChangeSwitchState;
+    }
+
+    void ChangeSwitchState(Lights light)
+    {
+        if (light != Light)
+            return;
+
+        if (currentSwitchState == OpenState.Open)
+        {
+            Close();
+            //TODO: SFX & EFFECTS FOR DISABLING DOOR OR TRAP
+        }
+        else if (currentSwitchState == OpenState.Close)
+        {
+            Open();
+            //TODO: SFX & EFFECTS FOR ENABLING DOOR OR TRAP
+        }
+    }
+
+    void Open()
+    {
+       // print("Open State");
+        itemCollider.enabled = false;
+        currentSwitchState = OpenState.Open;
+        dissolveController.isDissolving = true;
+
+    }
+
+    void Close()
+    {
+       // print("Close State");
+        itemCollider.enabled = true;
+        currentSwitchState = OpenState.Close;
+        dissolveController.isDissolving = false;
+    }
+
+    void OnDisable()
+    {
+        LightSensor.OnLightSensorTriggered -= ChangeSwitchState;
+    }
+}
