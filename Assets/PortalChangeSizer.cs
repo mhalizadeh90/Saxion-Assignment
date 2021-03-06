ddc817c3bc3292c5439adc858cc75244ff50d9d3
Mohammad Alizadeh <mhalizadeh@hotmail.com>
2021-03-06 12:22:19 +0330

    Finish Level 7

---
+++ Assets/PortalChangeSizer.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PortalChangeSizer : MonoBehaviour
+{
+    [SerializeField] float NewScaleSize;
+    void OnTriggerEnter2D(Collider2D collision)
+    {
+        OnSwitchSizePortalEnter?.Invoke(NewScaleSize);
+    }
+
+    public static Action<float> OnSwitchSizePortalEnter;
+}
---
+++ Assets/Rotator.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Rotator : MonoBehaviour
+{
+    [SerializeField] bool toRotateStepByStep;
+    [SerializeField] float RotateSpeed = 2;
+    [Header("Rotate Steps by Steps")]
+    [SerializeField] float DelayBetweenRotateAngle = 2;
+
+
+    void Start()
+    {
+        StartCoroutine(StartRotation());
+    }
+
+    IEnumerator StartRotation()
+    {
+        WaitForSeconds DelayBetweenSteps =  new WaitForSeconds(DelayBetweenRotateAngle);
+        float counter = 0;
+       
+        while (toRotateStepByStep)
+        {
+            transform.Rotate(Vector3.forward * Time.deltaTime * RotateSpeed);
+            counter += Time.deltaTime;
+            yield return null;
+
+            if (counter>= DelayBetweenRotateAngle)
+            {
+                counter = 0;
+                yield return DelayBetweenSteps;
+            }
+        }
+
+        while (!toRotateStepByStep)
+        {
+            transform.Rotate(Vector3.forward * Time.deltaTime * RotateSpeed);
+            yield return null;
+        }
+    }
+}
